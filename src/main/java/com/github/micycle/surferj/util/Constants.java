5f84fefda38510582ad71e9ded72800c3d15447a
Michael Carleton <micycle1@hotmail.co.uk>
2025-04-14 04:16:15 +0100

    delete old

--- src/main/java/com/github/micycle/surferj/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.github.micycle.surferj.util;
-
-public class Constants {
-    public static final double EPSILON = 1e-9; // Tolerance for floating point comparisons
-    public static final double DEFAULT_WEIGHT = 1.0;
-}
